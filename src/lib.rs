5eac47a2465bb213cc239f13b42ebb2460021686
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-04-11 13:28:53 +0000

    align me timbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,54 @@
-use std::mem::zeroed;
+#![allow(non_camel_case_types)]
+
+use std::path::Path;
 
 use dfdx::prelude::*;
 
-pub type V = Tensor1D<768>;
-pub type M = Tensor2D<768, 768>;
+use ::safetensors::SafeTensors;
+use memmap2::MmapOptions;
+use Tensor1D as V;
+use Tensor2D as M;
 
-fn mix(mix: V, p0: V, p1: V) -> V {
+fn mix<const N: usize>(mix: V<N>, p0: V<N>, p1: V<N>) -> V<N> {
     p1 * mix.clone() + p0 * (-mix + 1.)
 }
 
 #[derive(Clone)]
-pub struct ATT {
-    pub decay: V, // time_decay
-    pub bonus: V, // time_first
-    pub mix_k: V, // time_mix_k
-    pub mix_v: V, // time_mix_v
-    pub mix_r: V, // time_mix_r
-    pub wk: M,    // key.weight
-    pub wv: M,    // value.weight
-    pub wr: M,    // receptance.weight
-    pub wout: M,  // output.weight
+pub struct ATT<const N: usize> {
+    pub decay: V<N>,   // time_decay
+    pub bonus: V<N>,   // time_first
+    pub mix_k: V<N>,   // time_mix_k
+    pub mix_v: V<N>,   // time_mix_v
+    pub mix_r: V<N>,   // time_mix_r
+    pub wk: M<N, N>,   // key.weight
+    pub wv: M<N, N>,   // value.weight
+    pub wr: M<N, N>,   // receptance.weight
+    pub wout: M<N, N>, // output.weight
 }
 
 #[derive(Clone)]
-pub struct ATTState {
-    pub x: V,
-    pub num: V,
-    pub den: V,
+pub struct ATTState<const N: usize> {
+    pub x: V<N>,
+    pub num: V<N>,
+    pub den: V<N>,
 }
 
-impl ATT {
-    pub fn forward(&self, x: V, last: ATTState) -> (V, ATTState) {
+impl<const N: usize> ATT<N> {
+    pub fn zeros(dev: &Cpu) -> Self {
+        Self {
+            decay: dev.zeros(),
+            bonus: dev.zeros(),
+            mix_k: dev.zeros(),
+            mix_v: dev.zeros(),
+            mix_r: dev.zeros(),
+            wk: dev.zeros(),
+            wv: dev.zeros(),
+            wr: dev.zeros(),
+            wout: dev.zeros(),
+        }
+    }
+
+    pub fn forward(&self, x: V<N>, last: ATTState<N>) -> (V<N>, ATTState<N>) {
         let decay = self.decay.clone();
         let bonus = self.bonus.clone();
         let mix_k = self.mix_k.clone();
@@ -60,16 +78,25 @@ impl ATT {
 }
 
 #[derive(Clone)]
-pub struct FFN {
-    pub mix_k: V, // time_mix_k
-    pub mix_r: V, // time_mix_r
-    pub wk: M,    // key.weight
-    pub wv: M,    // value.weight
-    pub wr: M,    // receptance.weight
+pub struct FFN<const N: usize, const N1: usize> {
+    pub mix_k: V<N>,  // time_mix_k
+    pub mix_r: V<N>,  // time_mix_r
+    pub wk: M<N, N1>, // key.weight
+    pub wv: M<N1, N>, // value.weight
+    pub wr: M<N, N>,  // receptance.weight
 }
 
-impl FFN {
-    pub fn forward(&self, dev: &Cpu, x: V, last_x: V) -> (V, V) {
+impl<const N: usize, const N1: usize> FFN<N, N1> {
+    pub fn zeros(dev: &Cpu) -> Self {
+        Self {
+            mix_k: dev.zeros(),
+            mix_r: dev.zeros(),
+            wk: dev.zeros(),
+            wv: dev.zeros(),
+            wr: dev.zeros(),
+        }
+    }
+    pub fn forward(&self, dev: &Cpu, x: V<N>, last_x: V<N>) -> (V<N>, V<N>) {
         let mix_k = self.mix_k.clone();
         let mix_r = self.mix_r.clone();
         let wk = self.wk.clone();
@@ -84,34 +111,181 @@ impl FFN {
     }
 }
 
-// fn time_mixing(
-//     x: V,
-//     last_x: V,
-//     last_num: V,
-//     last_den: V,
-//     decay: V,
-//     bonus: V,
-//     mix_k: V,
-//     mix_v: V,
-//     mix_r: V,
-//     wk: M,
-//     wv: M,
-//     wr: M,
-//     wout: M,
-// ) -> (V, (V, V, V)) {
-//     let k = matmul(mix(mix_k, last_x.clone(), x.clone()), wk);
-//     let v = matmul(mix(mix_v, last_x.clone(), x.clone()), wv);
-//     let r = matmul(mix(mix_r, last_x, x.clone()), wr);
+#[derive(Clone)]
+pub struct LN<const N: usize> {
+    pub weight: V<N>,
+    pub bias: V<N>,
+}
 
-//     let _0 = exp(bonus + k.clone());
+impl<const N: usize> LN<N> {
+    pub fn zeros(dev: &Cpu) -> Self {
+        Self {
+            weight: dev.zeros(),
+            bias: dev.zeros(),
+        }
+    }
+}
 
-//     let wkv = (last_num.clone() + _0.clone() * v.clone()) / (last_den.clone() + _0);
-//     let rwkv = sigmoid(r) * wkv;
+#[derive(Clone)]
+pub struct RWKVBlock<const N_EMBED: usize, const N_EMBED_TIMES_4: usize> {
+    pub ln1: LN<N_EMBED>,
+    pub att: ATT<N_EMBED>,
+    pub ln2: LN<N_EMBED>,
+    pub ffn: FFN<N_EMBED, N_EMBED_TIMES_4>,
+}
 
-//     let _1 = exp(-exp(decay));
-//     let _2 = exp(k);
-//     let num = _1.clone() * last_num + _2.clone() * v;
-//     let den = _1 * last_den + _2;
+impl<const N_EMBED: usize, const N_EMBED_TIMES_4: usize> RWKVBlock<N_EMBED, N_EMBED_TIMES_4> {
+    pub fn zeros(dev: &Cpu) -> Self {
+        Self {
+            ln1: LN::zeros(dev),
+            att: ATT::zeros(dev),
+            ln2: LN::zeros(dev),
+            ffn: FFN::zeros(dev),
+        }
+    }
+}
 
-//     (matmul(rwkv, wout), (x, num, den))
-// }
+#[derive(Clone)]
+pub struct RWKV<
+    const N_LAYER: usize,
+    const N_EMBED: usize,
+    const N_EMBED_TIMES_4: usize,
+    const N_VOCAB: usize,
+> {
+    pub emb: Tensor2D<N_VOCAB, N_EMBED>, // emb.weight
+    pub ln_in: LN<N_EMBED>,              // blocks.0.ln0
+    pub blocks: [RWKVBlock<N_EMBED, N_EMBED_TIMES_4>; N_LAYER],
+    pub ln_out: LN<N_EMBED>,              // ln_out
+    pub head: Tensor2D<N_VOCAB, N_EMBED>, // head.weight
+}
+
+impl<
+        const N_LAYER: usize,
+        const N_EMBED: usize,
+        const N_EMBED_TIMES_4: usize,
+        const N_VOCAB: usize,
+    > RWKV<N_LAYER, N_EMBED, N_EMBED_TIMES_4, N_VOCAB>
+{
+    pub fn zeros(dev: &Cpu) -> Self {
+        Self {
+            emb: dev.zeros(),
+            ln_in: LN::zeros(dev),
+            blocks: std::array::from_fn(|_| RWKVBlock::zeros(dev)),
+            ln_out: LN::zeros(dev),
+            head: dev.zeros(),
+        }
+    }
+
+    pub fn load_safetensors<P: AsRef<Path>>(
+        &mut self,
+        path: P,
+    ) -> Result<(), dfdx::tensor::safetensors::Error> {
+        let f = std::fs::File::open(path)?;
+        let buffer = unsafe { MmapOptions::new().map(&f)? };
+        let xs = SafeTensors::deserialize(&buffer)?;
+
+        let Self {
+            emb,
+            ln_in,
+            blocks,
+            ln_out,
+            head,
+        } = self;
+        _cp(&xs, "emb.weight", emb)?;
+        _cp(&xs, "head.weight", head)?;
+        _cp_ln(&xs, "blocks.0.ln0", ln_in)?;
+        _cp_ln(&xs, "ln_out", ln_out)?;
+        for (i, block) in blocks.into_iter().enumerate() {
+            _cp_block(&xs, &format!("blocks.{i}"), block)?;
+        }
+
+        Ok(())
+    }
+}
+
+fn _cp_block<const N_EMBED: usize, const N_EMBED_TIMES_4: usize>(
+    xs: &SafeTensors,
+    prefix: &str,
+    block: &mut RWKVBlock<N_EMBED, N_EMBED_TIMES_4>,
+) -> Result<(), dfdx::tensor::safetensors::Error> {
+    let RWKVBlock { ln1, att, ln2, ffn } = block;
+
+    _cp_ln(xs, &format!("{prefix}.ln1"), ln1)?;
+    _cp_ln(xs, &format!("{prefix}.ln2"), ln2)?;
+
+    {
+        let ATT {
+            decay,
+            bonus,
+            mix_k,
+            mix_v,
+            mix_r,
+            wk,
+            wv,
+            wr,
+            wout,
+        } = att;
+        _cp(xs, &format!("{prefix}.att.time_decay"), decay)?;
+        _cp(xs, &format!("{prefix}.att.time_first"), bonus)?;
+        _cp(xs, &format!("{prefix}.att.time_mix_k"), mix_k)?;
+        _cp(xs, &format!("{prefix}.att.time_mix_v"), mix_v)?;
+        _cp(xs, &format!("{prefix}.att.time_mix_r"), mix_r)?;
+        _cp(xs, &format!("{prefix}.att.key.weight"), wk)?;
+        _cp(xs, &format!("{prefix}.att.value.weight"), wv)?;
+        _cp(xs, &format!("{prefix}.att.receptance.weight"), wr)?;
+        _cp(xs, &format!("{prefix}.att.output.weight"), wout)?;
+    }
+
+    {
+        let FFN {
+            mix_k,
+            mix_r,
+            wk,
+            wv,
+            wr,
+        } = ffn;
+        _cp(xs, &format!("{prefix}.ffn.time_mix_k"), mix_k)?;
+        _cp(xs, &format!("{prefix}.ffn.time_mix_r"), mix_r)?;
+        _cp(xs, &format!("{prefix}.ffn.key.weight"), wk)?;
+        _cp(xs, &format!("{prefix}.ffn.value.weight"), wv)?;
+        _cp(xs, &format!("{prefix}.ffn.receptance.weight"), wr)?;
+    }
+
+    Ok(())
+}
+
+fn _cp_ln<const N: usize>(
+    xs: &SafeTensors,
+    prefix: &str,
+    ln_in: &mut LN<N>,
+) -> Result<(), dfdx::tensor::safetensors::Error> {
+    _cp(xs, &format!("{prefix}.weight"), &mut ln_in.weight)?;
+    _cp(xs, &format!("{prefix}.bias"), &mut ln_in.bias)?;
+    Ok(())
+}
+
+/// copy tensor
+fn _cp<S: Shape, D: CopySlice<f32>, T>(
+    tensors: &SafeTensors,
+    tensor_name: &str,
+    tensor: &mut Tensor<S, f32, D, T>,
+) -> Result<(), dfdx::tensor::safetensors::Error> {
+    let data = &tensors.tensor(tensor_name)?.data();
+    tensor.copy_from(unsafe { align_to_f32(data) });
+    Ok(())
+}
+
+/// data may be unaligned
+unsafe fn align_to_f32(data: &[u8]) -> &[f32] {
+    std::slice::from_raw_parts(data.as_ptr() as *const f32, data.len() / 4)
+}
+
+pub type RWKV_430m = RWKV<24, 1024, 4096, 50277>;
+
+#[test]
+fn test_load_model() -> Result<(), dfdx::tensor::safetensors::Error> {
+    let dev: Cpu = Default::default();
+    let mut model = RWKV_430m::zeros(&dev);
+    model.load_safetensors("RWKV-4-Pile-430M-20220808-8066.safetensors")?;
+    Ok(())
+}
